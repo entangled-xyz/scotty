a3bb7650816e8d4a7ecbbc246e9bc35730f4300f
Vasily Vasinov <des.elyon@gmail.com>
2019-05-31 15:23:06 -0600

    Add `SuperpositionReader` and qubit and state readouts there.

--- src/main/scala/scotty/quantum/State.scala
+++ src/main/scala/scotty/quantum/State.scala
@@ -15,33 +15,12 @@ trait Superposition extends State {
 
   def probabilities: Seq[Double] = vector.map(s => Math.pow(s.abs().roundWithPrecision, 2))
 
-  def stateProbabilities: Seq[(Seq[Int], Complex, Double)] = vector.zipWithIndex.map(pair => (
-    MathUtils.toBinaryPadded(pair._2, qubitCount),
-    pair._1,
-    Math.pow(pair._1.abs().roundWithPrecision, 2)
-  )).toSeq
-
-  def qubitProbabilities: Seq[(Int, Double)] = {
-    val ps = stateProbabilities
-
-    (0 until qubitCount).map(q => {
-      (q, ps.foldLeft(0d)((sum, pair) => if (pair._1(q) == 1) sum + pair._3 else sum))
-    })
-  }
-
   def par(state: Superposition): Superposition
 
   def measure: Collapsed
 
-  def toStateString: String = stateProbabilities
-    .map(p => s"|${p._1.mkString("")}>: Amplitude: ${p._2}, P: ${p._3.roundWithPrecision.toPercent}%")
-    .mkString("\n")
-
-  def toQubitString: String = qubitProbabilities
-    .map(p => s"qubits[${p._1}] == 1 => probability ${p._2.roundWithPrecision.toPercent}%")
-    .mkString("\n")
-
-  def toString(showState: Boolean): String = if (showState) toStateString else toQubitString
+  def toString(fullState: Boolean): String =
+    if (fullState) StateReader(this).toString else QubitReader(this).toString
 
   override def toString: String = toString(true)
 }
---
+++ src/main/scala/scotty/quantum/SuperpositionReader.scala
@@ -0,0 +1,36 @@
+package scotty.quantum
+
+import scotty.quantum.math.{Complex, MathUtils}
+import scotty.quantum.math.MathUtils._
+
+sealed trait SuperpositionReader[T] {
+  val state: Superposition
+
+  def read: Seq[T]
+}
+
+case class StateReader(state: Superposition) extends SuperpositionReader[(Seq[Int], Complex, Double)] {
+  def read: Seq[(Seq[Int], Complex, Double)] = state.vector.zipWithIndex.map(pair => (
+    MathUtils.toBinaryPadded(pair._2, state.qubitCount),
+    pair._1,
+    Math.pow(pair._1.abs().roundWithPrecision, 2)
+  )).toSeq
+
+  override def toString: String = read
+    .map(p => s"|${p._1.mkString("")}>: Amplitude: ${p._2}, P: ${p._3.roundWithPrecision.toPercent}%")
+    .mkString("\n")
+}
+
+case class QubitReader(state: Superposition) extends SuperpositionReader[(Int, Double)] {
+  def read: Seq[(Int, Double)] = {
+    val ps = StateReader(state).read
+
+    (0 until state.qubitCount).map(q => {
+      (q, ps.foldLeft(0d)((sum, pair) => if (pair._1(q) == 1) sum + pair._3 else sum))
+    })
+  }
+
+  override def toString: String = read
+    .map(p => s"qubits[${p._1}] == 1 => probability ${p._2.roundWithPrecision.toPercent}%")
+    .mkString("\n")
+}
