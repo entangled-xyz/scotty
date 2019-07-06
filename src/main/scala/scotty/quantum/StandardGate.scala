299666a322a0d9e3d1fe1164449f4b0ccdea08b0
Vasily Vasinov <des.elyon@gmail.com>
2019-07-06 16:06:57 -0600

    Make `Op` traits more lean and move implementation-specific bits to `QuantumSimulator`, replace different gate matrix methods in `QuantumContext` with a single `gateMatrix` method delegating implementation details to the descendants (e.g., `QuantumSimulator`), add CustomGate with simplified initialization.

--- src/main/scala/scotty/quantum/StandardGate.scala
+++ src/main/scala/scotty/quantum/StandardGate.scala
@@ -1,37 +1,50 @@
 package scotty.quantum
 
 object StandardGate {
-  case class Controlled(controlIndex: Int, target: Gate) extends Control
+  case class Controlled(controlIndex: Int, target: Gate) extends ControlGate
 
-  case class H(index1: Int) extends Target
+  case class H(index: Int) extends TargetGate {
+    val indexes: Seq[Int] = Seq(index)
+  }
 
-  case class I(index1: Int) extends Target
+  case class I(index: Int) extends TargetGate {
+    val indexes: Seq[Int] = Seq(index)
+  }
 
-  case class X(index1: Int) extends Target
+  case class X(index: Int) extends TargetGate {
+    val indexes: Seq[Int] = Seq(index)
+  }
 
-  case class Y(index1: Int) extends Target
+  case class Y(index: Int) extends TargetGate {
+    val indexes: Seq[Int] = Seq(index)
+  }
 
-  case class Z(index1: Int) extends Target
+  case class Z(index: Int) extends TargetGate {
+    val indexes: Seq[Int] = Seq(index)
+  }
 
-  case class RX(theta: Double, index1: Int) extends Target {
+  case class RX(theta: Double, index: Int) extends TargetGate {
+    val indexes: Seq[Int] = Seq(index)
     override val params: Seq[Double] = Seq(theta)
   }
 
-  case class RY(theta: Double, index1: Int) extends Target {
+  case class RY(theta: Double, index: Int) extends TargetGate {
+    val indexes: Seq[Int] = Seq(index)
     override val params: Seq[Double] = Seq(theta)
   }
 
-  case class RZ(theta: Double, index1: Int) extends Target {
+  case class RZ(theta: Double, index: Int) extends TargetGate {
+    val indexes: Seq[Int] = Seq(index)
     override val params: Seq[Double] = Seq(theta)
   }
 
-  case class CNOT(controlIndex: Int, targetIndex: Int) extends Control {
+  case class CNOT(controlIndex: Int, targetIndex: Int) extends ControlGate {
     lazy val target = X(targetIndex)
   }
 
-  case class CCNOT(controlIndex: Int, controlIndex2: Int, targetIndex: Int) extends Control {
+  case class CCNOT(controlIndex: Int, controlIndex2: Int, targetIndex: Int) extends ControlGate {
     lazy val target = Controlled(controlIndex2, X(targetIndex))
   }
 
-  case class SWAP(index1: Int, index2: Int) extends QubitSwap
+  case class SWAP(index1: Int, index2: Int) extends SwapGate
 }
--- src/test/scala/scotty/simulator/CustomGatesSpec.scala
+++ src/test/scala/scotty/simulator/CustomGatesSpec.scala
@@ -6,16 +6,22 @@ import scotty.quantum.math.Complex
 
 class CustomGatesSpec extends FlatSpec {
   val sim = QuantumSimulator()
+  val matrix = Array(
+    Array(Complex(0), Complex(1)),
+    Array(Complex(1), Complex(0))
+  )
 
-  "Custom gate" should "negate a qubit" in {
-    case class CustomGate(index1: Int) extends Target {
-      override def targetMatrix = Some(Array(
-        Array(Complex(0), Complex(1)),
-        Array(Complex(1), Complex(0))
-      ))
-    }
+  "Custom gate" should "be able to be defined through inheritance" in {
+    case class MyGate(index: Int) extends CustomGate(matrix, index)
 
-    assert(sim.runAndMeasure(Circuit(CustomGate(0))).toBinaryRegister.values == Seq(One()))
-    assert(sim.runAndMeasure(Circuit(CustomGate(0)).withRegister(Qubit.one)).toBinaryRegister.values == Seq(Zero()))
+    assert(sim.runAndMeasure(Circuit(MyGate(0))).toBinaryRegister.values == Seq(One()))
+    assert(sim.runAndMeasure(Circuit(MyGate(0)).withRegister(Qubit.one)).toBinaryRegister.values == Seq(Zero()))
+  }
+
+  it should "be able to be defined as a method" in {
+    def myGate(index: Int) = new CustomGate(matrix, index)
+
+    assert(sim.runAndMeasure(Circuit(myGate(0))).toBinaryRegister.values == Seq(One()))
+    assert(sim.runAndMeasure(Circuit(myGate(0)).withRegister(Qubit.one)).toBinaryRegister.values == Seq(Zero()))
   }
 }
